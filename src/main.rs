f6bab81bff9125155a0f88e1d43eca97e2af7592
vvesllo <timyr2005kadirov@gmail.com>
2025-05-28 03:35:10 +0500

    hello world text display

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,84 @@
-use sdl3::Sdl;
-use sdl3::VideoSubsystem;
-use sdl3::EventPump;
-
-use sdl3::video::Window;
-use sdl3::render::{Canvas, FRect};
-
-use sdl3::pixels::Color;
+use sdl3::{EventPump, Sdl, VideoSubsystem};
+use sdl3::render::{Canvas, FRect, Texture, TextureCreator};
+use sdl3::video::{Window, WindowContext};
 use sdl3::event::Event;
-use sdl3::keyboard::Scancode;
-use sdl3::image::LoadTexture;
 
-use std::time::Duration;
+use sdl3::keyboard::Keycode;
+use sdl3::pixels::Color;
+use sdl3::surface::Surface;
 
-mod Vector;
-use Vector::Vector2;
+use sdl3::ttf::{Sdl3TtfContext, FontStyle, Font};
 
-fn main()
-{
-    let sdl_context: Sdl = sdl3::init().unwrap();
+use std::path::Path;
+use std::thread;
+use std::time;
 
+
+// это пиздец
+fn render(canvas: &mut Canvas<Window>, texture_creator: &TextureCreator<WindowContext>, font: &Font) -> Result<(), String> {
+    let color: Color = Color::RGB(0x34, 0x40, 0x64);
+    canvas.set_draw_color(color);
+    canvas.clear();
+
+    let hello_text: String = String::from("Hello, world!");
+    let surface: Surface<'_> = font
+        .render(&hello_text)
+        .blended(Color::RGB(0xA7,0xB1,0xCC))
+        .unwrap();
+    let texture: Texture<'_>= texture_creator
+        .create_texture_from_surface(&surface)
+        .unwrap();
+    let target: FRect = FRect::new(10., 10., 200., 100.);
+    canvas
+        .copy(&texture, None, Some(target))
+        .unwrap();
+        
+    canvas.present();
+    Ok(())
+}
+
+
+fn main() ->Result<(), String> {
+    let sdl_context: Sdl = sdl3::init().unwrap(); 
     let video_subsystem: VideoSubsystem = sdl_context
         .video()
         .unwrap();
 
     let window: Window = video_subsystem
-        .window("SDL Project", 720, 480)
-        .position_centered()
+        .window("SDL3", 720, 480)
         .build()
         .unwrap();
 
-    let mut canvas: Canvas<Window> = window
-        .into_canvas();
+    let mut canvas: Canvas<Window> = window.into_canvas();
+    let texture_creator: TextureCreator<WindowContext>= canvas.texture_creator();
+
+    let ttf_context: Sdl3TtfContext = sdl3::ttf::init()
+        .unwrap();
+    
+    let font_path: &Path = Path::new(&"fonts/font.otf");
+    let mut font: Font<'_, 'static> = ttf_context
+        .load_font(font_path, 128f32).unwrap();
+    font.set_style(FontStyle::NORMAL);
+
     let mut event_pump: EventPump = sdl_context
         .event_pump()
         .unwrap();
-
-
-    let texture_creator = canvas
-        .texture_creator();
-   
-    let texture = texture_creator
-        .load_texture("./horse.jpg")
-        .unwrap();
-    
-    texture.scale_mode();
-
-    canvas.set_draw_color(Color::RGB(0x34, 0x40, 0x64));
-
-    let mut position: Vector2<f32> = Vector2::<f32>::new(0., 0.);
-
-    let mut rect: FRect = FRect::new(0., 0., 128., 128.);
-
-    canvas
-        .copy(&texture, None, None)
-        .unwrap();
-    canvas.present();
-
-
+ 
     'running: loop {
-        canvas.clear();
-
-        for event in event_pump.poll_iter()
-        {
-            match event
-            {
-                Event::Quit { .. } => { break 'running; },
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running;  
+                },
                 _ => {}
             }
         }
-        
 
-        let mut velocity: Vector2<f32> = Vector2::<f32>::new(0., 0.);
-        
-        let keyboard_state = event_pump
-            .keyboard_state();
+        render(&mut canvas, &texture_creator, &font).unwrap();
 
-        
-        if keyboard_state.is_scancode_pressed(Scancode::W) {
-            velocity.y -= 1.;
-        }
-        if keyboard_state.is_scancode_pressed(Scancode::S) {
-            velocity.y += 1.;
-        }
-        
-        if keyboard_state.is_scancode_pressed(Scancode::A) {
-            velocity.x -= 1.;
-        }
-        if keyboard_state.is_scancode_pressed(Scancode::D) {
-            velocity.x += 1.;
-        }
+        thread::sleep(time::Duration::new(0, 1_000_000_000u32 / 60));
+    }
 
-
-        position += velocity;
-
-        rect.set_x(position.x);
-        rect.set_y(position.y);
-
-        canvas
-            .copy_ex(
-                &texture,
-                None,
-                Some(rect),
-                0.,
-                None,
-                false,
-                false,
-            ).unwrap();
-        canvas.present();
-
-
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    };
+    Ok(())
 }
